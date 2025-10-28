4ae00b251f97c053a0c64332904bd8b9eb00a4c6
Sander Declerck <sander@sdsoftware.be>
2025-10-28 12:08:24 +0100

    Disable nock

--- index.test.js
+++ index.test.js
@@ -2,8 +2,8 @@ const axios = require("axios");
 const nock = require("nock");
 const { fetchData } = require(".");
 
-axios.defaults.adapter = "http";
-nock.disableNetConnect();
+// axios.defaults.adapter = "http";
+// nock.disableNetConnect();
 
 // delete process.env.HTTP_PROXY;
 // delete process.env.HTTPS_PROXY;
@@ -16,7 +16,7 @@ describe("HTTP request test", () => {
   test("should make a GET request to httpbin and return status 200", async () => {
     // Mock the HTTP request
 
-    const scope = nock("https://httpbin.org").get("/status/200").reply(200);
+    // const scope = nock("https://httpbin.org").get("/status/200").reply(200);
 
     const result = await fetchData();
 
