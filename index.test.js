e12fc68482ea86bbd4c269da57ad36ec5c2d2a36
Sander Declerck <sander@sdsoftware.be>
2025-10-28 11:31:25 +0100

    Try and unset proxy

--- index.test.js
+++ index.test.js
@@ -5,6 +5,9 @@ const { fetchData } = require(".");
 axios.defaults.adapter = "http";
 nock.disableNetConnect();
 
+delete process.env.HTTP_PROXY;
+delete process.env.HTTPS_PROXY;
+
 describe("HTTP request test", () => {
   afterEach(() => {
     nock.cleanAll();
@@ -12,7 +15,6 @@ describe("HTTP request test", () => {
 
   test("should make a GET request to httpbin and return status 200", async () => {
     // Mock the HTTP request
-    console.log(process.env.HTTPS_PROXY);
 
     const scope = nock("https://httpbin.org").get("/status/200").reply(200);
 
